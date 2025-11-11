582316fdffa7236b7246addd1879ae5a404ca63b
Mathew <mneves@uvm.edu>
2025-11-11 13:46:38 -0500

    Update rgb_to_hex.py

--- programming files/rgb_to_hex.py
+++ programming files/rgb_to_hex.py
@@ -1,8 +1,11 @@
+# Bugs introduced: MN
+
+// Converts RGB to Hex
 def rgb_to_hex(r, g, b):
-    r = max(0, min(255, r))
-    g = max(0, min(255, g))
-    b = max(0, min(255, b))
-    return '{:02X}{:02X}{:02X}'.format(r, g, b)
+    r = max(0, min(255, r));
+    g = max(0, min(255, g));
+    b = max(0, min(255, b));
+    return '{:02X}{:02X}{:02X}'.format(b, g, r)
 
 # Jacob Pancoast
 
