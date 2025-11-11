2f7355325e2fb162f9677db5d82d42a222d1896b
IsaacYoung981 <156706677+IsaacYoung981@users.noreply.github.com>
2025-11-11 13:46:46 -0500

    Update datetime_convert.py
    Broke everything :)

--- programming files/datetime_convert.py
+++ programming files/datetime_convert.py
@@ -1,9 +1,8 @@
+# Isaac Young
 from datetime import datetime
 
 date_str = "2022-03-17 10:45:30"
-date_obj = datetime.strptime(date_str, '%Y-%m-%d %H:%M:%S')
+date_obj = datetime.strptime(date_str + "Hello your broken", '%Y-%m-%d %y:%M:%Y')
 formatted_date = date_obj.strftime('%m/%d/%Y %H:%M:%S')
 
 print(formatted_date)
-
-# Isaac Young
