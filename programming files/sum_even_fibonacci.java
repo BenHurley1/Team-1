dc877448f375a1d667ff96a4536d5b6cc3bbe9d6
Ellie <ejgookin@gmail.com>
2025-11-11 13:45:17 -0500

    broke sum_even_fibonacci.java

--- programming files/sum_even_fibonacci.java
+++ programming files/sum_even_fibonacci.java
@@ -1,10 +1,16 @@
+// Bugs added:
+// b <= 4000000 instead of b < 4000000
+// b % 2 != 0 instead of b % 2 == 0
+// sumEven = b instead of sumEven += b
+// -EG
+
 public class sum_even_fibonacci {
     public static void main(String[] args) {
         int a = 0, b = 1;
         int sumEven = 0;
-        while (b < 4000000) {
-            if (b % 2 == 0) {
-                sumEven += b;
+        while (b <= 4000000) {
+            if (b % 2 != 0) {
+                sumEven = b;
             }
             int temp = b;
             b = a + b;
